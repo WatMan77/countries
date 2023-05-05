949eb6de551a639b8313ab8f59fc6c99bd49799b
Daniel Kaluski <dansku110@gmail.com>
2023-05-05 13:37:58 +0300

    Optimized fetching, save already visited countries in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,18 @@ import CountryInfoDetails from './CountryDetails'
 
 const App = () => {
   const [country, setCountry] = useState("")
-  const [countriesInfo, setCountriesInfo] = useState([])
   const [countryInfo, setCountryInfo] = useState()
+  const [countryNames, setCountryNames] = useState([])
 
   // Fetch all the data of the countries and save it
   useEffect(() => {
     const fetchCountries = async () => {
       try {
-        const response = await fetch("https://restcountries.com/v3.1/all")
+        const response = await fetch("https://restcountries.com/v3.1/all?fields=name")
         const data = await response.json();
 
         // Change the data so the "Select" component can use the names in the dropdown
-        setCountriesInfo(
+        setCountryNames(
           data.map(d => {
             return { ...d, value: d.name.common, label: d.name.common }
           }).sort((a, b) => a.value < b.value ? -1 : 1)
@@ -31,18 +31,37 @@ const App = () => {
   }, [])
 
   useEffect(() => {
-    if (country) {
-      const foundCountry = countriesInfo.find(c => c.name.common === country)
-      if (foundCountry) {
-        setCountryInfo(foundCountry)
+    const fetchCountryInfo = async () => {
+      if (!country) {
+        return
+      }
+      //Check local storage for the country info
+      const countryData = JSON.parse(localStorage.getItem(country))
+      if (countryData) {
+        setCountryInfo(countryData)
+        return
+      }
+
+      try {
+        const response = await fetch(`https://restcountries.com/v3.1/name/${country}?fullText=true`)
+        const foundCountry = await response.json()
+        if (foundCountry.length > 0) {
+          localStorage.setItem(country, JSON.stringify(foundCountry[0]))
+          setCountryInfo(foundCountry[0])
+        }
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.log("Error in fetching country info", e)
       }
     }
 
-  }, [country, countriesInfo])
+    fetchCountryInfo()
+
+  }, [country])
 
   return (
     <>
-      <CountriesDropdown countryNames={countriesInfo} setCountry={setCountry} />
+      <CountriesDropdown countryNames={countryNames} setCountry={setCountry} />
       <CountryInfoDetails countryInfo={countryInfo} />
     </>
   )
